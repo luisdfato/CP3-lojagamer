c23289debd1969b65f1ff4cd1b344da5ea640903
Luis <luisfernandolemecampos2007@gmail.com>
2026-05-23 16:33:18 -0300

    feat: reformular visual da nexus gamer com intro animada

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const intro = document.querySelector('.intro')
+
+window.addEventListener('load', () => {
+
+    setTimeout(() => {
+
+        intro.classList.add('intro-hide')
+
+    }, 2200)
+
+})
